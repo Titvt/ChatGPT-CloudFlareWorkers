ef5223dcb7a9cae8c240abdf50b924c6f8212788
Titvt <tqydyqt@126.com>
2023-05-04 02:17:05 +0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # ChatGPT-CloudFlareWorkers
 
-Enjoy ChatGPT on CloudFlare Workers!
+**Enjoy ChatGPT on CloudFlare Workers!**
 
-The only thing you need to do is copying your OpenAI API key to API_KEY, and then you can deploy it on CloudFlare.
+## Deploy
+
+Before deploying your workers, you should add environment variable `API_KEY` and fill it as your OpenAI API KEY.
+
+## Usage
+
+### Chat
+
+The request router is `/chat`, and the request method is `POST`.
+
+You can specify some request parameters through headers:
+- temperature
+
+You can either specify a plain text or a JSON text in the request body.
+
+If you specify a plain text, it will be automatically converted to a simple chat message.
+
+If you specify a JSON text, it will be parsed as a chat message.
+
+[View the description of the chat message](https://platform.openai.com/docs/api-reference/chat)
+
+### Images
+
+The request router is `/images`, and the request method is `POST`.
+
+You can specify some request parameters through headers:
+- n
+- size
+- response_format
+
+Specify the prompt in the request body.
--- production.js
+++ production.js
@@ -1,35 +1,47 @@
-const API_KEY = ''
-
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     if (request.method !== 'POST') {
-      return new Response('Only POST method is supported.')
+      return new Response('Invalid Method', { status: 404 })
     }
 
-    let prompt = await request.text()
+    switch (new URL(request.url).pathname) {
+      case '/chat':
+        return this.chat(request, env)
+      case '/images':
+        return this.images(request, env)
+      default:
+        return new Response('Invalid Router', { status: 404 })
+    }
+  },
 
-    if (prompt.length === 0 || prompt.length > 2048) {
-      return new Response('The length of the payload should be in (0,2048].')
+  async chat(request, env) {
+    let text = await request.text()
+    let messages
+
+    try {
+      messages = JSON.parse(text)
+    } catch {
+      messages = [{ role: 'user', content: text }]
     }
 
-    let url = 'https://api.openai.com/v1/completions'
+    let temperature = parseFloat(request.headers.get('temperature'))
+
+    if (isNaN(temperature)) {
+      temperature = 1
+    }
+
+    let url = 'https://api.openai.com/v1/chat/completions'
     let options = {
       method: 'POST',
       headers: [
-        [
-          'Content-Type',
-          'application/json',
-        ],
-        [
-          'Authorization',
-          'Bearer ' + API_KEY,
-        ],
+        ['Content-Type', 'application/json'],
+        ['Authorization', `Bearer ${env.API_KEY}`],
       ],
       body: JSON.stringify(
         {
-          model: 'text-davinci-003',
-          prompt: prompt,
-          max_tokens: 2048,
+          model: 'gpt-3.5-turbo',
+          messages: messages,
+          temperature: temperature,
         },
       ),
     }
@@ -37,9 +49,45 @@ export default {
     let data = JSON.parse(await response.text())
 
     if (!data.choices) {
-      return new Response('An error is occurred during the request.')
+      return new Response(data.error.message, { status: 400 })
     }
 
-    return new Response(data.choices[0].text.trim())
-  }
+    return new Response(data.choices[0].message.content.trim())
+  },
+
+  async images(request, env) {
+    let prompt = await request.text()
+    let n = parseInt(request.headers.get('n'))
+
+    if (isNaN(n)) {
+      n = 1
+    }
+
+    let size = request.headers.get('size') ?? '1024x1024'
+    let response_format = request.headers.get('response_format') ?? 'url'
+    let url = 'https://api.openai.com/v1/images/generations'
+    let options = {
+      method: 'POST',
+      headers: [
+        ['Content-Type', 'application/json'],
+        ['Authorization', `Bearer ${env.API_KEY}`],
+      ],
+      body: JSON.stringify(
+        {
+          prompt: prompt,
+          n: n,
+          size: size,
+          response_format: response_format,
+        },
+      ),
+    }
+    let response = await fetch(new Request(url, options))
+    let data = JSON.parse(await response.text())
+
+    if (!data.data) {
+      return new Response(data.error.message, { status: 400 })
+    }
+
+    return new Response(JSON.stringify(data.data))
+  },
 }
